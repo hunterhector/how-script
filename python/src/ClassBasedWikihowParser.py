ece65321ec2b3139daa055dce3be16f3320b79f1
hunterhector <hunterhector@gmail.com>
2014-09-07 00:15:12 -0400

    Retain references between wikihow

--- python/src/ClassBasedWikihowParser.py
+++ python/src/ClassBasedWikihowParser.py
@@ -25,11 +25,25 @@ def is_category_link(tag):
 
 
 def parse_category(tag):
-    return map(lambda a: a.get_text(), tag.find_all(is_category_link))
+    c_nodes = []
+    for a in tag.find_all(is_category_link):
+        c = etree.Element('c')
+        c.text = a.get_text()
+        c_nodes.append(c)
+    return c_nodes
 
 
 def parse_categories(soup):
-    return map(lambda category : parse_category(category), soup.find_all(is_category_list))
+    categories = etree.Element('categories')
+
+    for cs in soup.find_all(is_category_list):
+        category = etree.SubElement(categories,'category')
+        for c in parse_category(cs):    
+            category.append(c)
+    
+    return categories
+
+    #return map(lambda category : parse_category(category), soup.find_all(is_category_list))
 
 
 def is_step_listing(tag):
@@ -44,65 +58,144 @@ def is_clear_div(tag):
     return tag.name == 'div' and tag.has_attr('class') and tag['class'][0] == 'clearall'
 
 
-def parse_ul_text(tag):
-    return map(lambda l : unicode(l.get_text()).rstrip(), tag.find_all(lambda t : t.name == 'li'))
+def is_tips_div(tag):
+    return tag.name == 'div' and tag.has_attr('id') and tag['id'] == 'tips'
 
+
+def parse_ul_text(tag):
+    ls = etree.Element('ul')
+    for l in tag.find_all(lambda t : t.name == 'li'):
+        li = etree.SubElement(ls,"li")
+        li.text = l.get_text()
+
+    return ls
+
+def create_text_node(s):
+    n = etree.Element('text')
+    n.text = s
+    return n
 
 def parse_text(tag):
     if type(tag) is NavigableString:
-        return unicode(tag.string)
+        return create_text_node(unicode(tag.string))
     elif type(tag) is Tag:
         if tag.name == 'ul':
             return parse_ul_text(tag)
+        elif tag.name == 'a':
+            ref = tag['href'] if tag.has_attr('href') else ''
+            title = tag['title'] if tag.has_attr('title') else ''
+            anchor = etree.Element('a')
+            anchor.set('href' ,ref)
+            anchor.set('title' ,title)
+            anchor.text = tag.get_text()
+            return anchor
         else:
-            return tag.get_text()
+            return create_text_node(tag.get_text())
 
-def parse_step_details(tag):
+def get_text_from_tag_group(tags):
     s = []
-    while not tag is None and not is_clear_div(tag):
-        s.append(parse_text(tag))
-        tag = tag.next_sibling
+    last_tag_name = None
+    for tag in tags:
+        current_tag_name = 'NavigableString' if type(tag) is NavigableString else tag.name
+            
+        if last_tag_name == None:
+            s.append(parse_text(tag))
+        elif current_tag_name == 'NavigableString' and last_tag_name == 'NavigableString':
+            s.append(parse_text(tag))
+        elif current_tag_name == 'a' and (last_tag_name == 'NavigableString' or last_tag_name == 'a'):
+            s.append(parse_text(tag))
+            
     return s
 
 
+def parse_step_details(tag):
+    tags_to_parse = []
+    while not tag is None and not is_clear_div(tag):
+        tags_to_parse.append(tag)
+        tag = tag.next_sibling
+    return get_text_from_tag_group(tags_to_parse)    
+    
+
+
 def parse_step_listing(tag):
-    return map(lambda b: [b.get_text(),parse_step_details(b.next_sibling)], tag.find_all(is_step_tag))
+    steps = []
+    for step_em_tag in tag.find_all(is_step_tag):
+        step = etree.Element('step')
+        em = etree.SubElement(step,'em')
+        em.text = step_em_tag.get_text()
+
+        details = etree.SubElement(step,'details')
+        for detail in parse_step_details(step_em_tag.next_sibling):
+            details.append(detail)
+
+        steps.append(step)
+        
+    return steps
+
+    #return map(lambda b: [b.get_text(),parse_step_details(b.next_sibling)], tag.find_all(is_step_tag))
 
 
 def parse_step_listings(soup):
-    return map(lambda listing: parse_step_listing(listing),  soup.find_all(is_step_listing))
+    methods = etree.Element('methods')
+
+    for listing in soup.find_all(is_step_listing):
+        methods = etree.SubElement(methods,'method')
+        for s in parse_step_listing(listing):
+            methods.append(s)
+
+    return methods
+   # return map(lambda listing: parse_step_listing(listing),  soup.find_all(is_step_listing))
 
 
-def format_output(out, all_categories, all_step_listings):
+def parse_tip(tag):
+    return map(lambda tip: parse_text(tip), tag.find_all(lambda t : t.name == 'li'))
+
+
+def parse_tips(soup):
+    return map(lambda tips : parse_tip(tips), soup.find_all(is_tips_div))
+
+
+def format_output(out, all_categories, all_step_listings, all_tips):
     root = etree.Element('root')
    
     doc = etree.SubElement(root,'document')
 
-    categories = etree.SubElement(doc,'categories')
+    doc.append(all_categories)
+   # categories = etree.SubElement(doc,'categories')
 
-    for cs in all_categories:
-        category = etree.SubElement(categories,'category')
-        for c in cs:
-            ontology = etree.SubElement(category,'c')
-            ontology.text = c
+   # for cs in all_categories:
+   #     category = etree.SubElement(categories,'category')
+   #     for c in cs:
+   #         ontology = etree.SubElement(category,'c')
+   #         ontology.text = c
 
-    for sls in all_step_listings:
-        steps = etree.SubElement(doc,'steps')
+    doc.append(all_step_listings)
 
-        for s in sls:
-            step = etree.SubElement(steps,'step')
-            em = etree.SubElement(step, 'em')
-            em.text = s[0]
+#   methods = etree.SubElement(doc,'methods')
+#    for sls in all_step_listings:
+#        steps = etree.SubElement(methods,'method')
 
-            details = etree.SubElement(step,'details')
-            for detail_stuff in s[1]:
-                detail = etree.SubElement(details,'d')
-                if isinstance(detail_stuff,list):
-                    for l in detail_stuff:
-                        li = etree.SubElement(detail,'l')
-                        li.text = l
-                else:
-                    detail.text = detail_stuff
+   #     for s in sls:
+    #        step = etree.SubElement(steps,'step')
+      #      em = etree.SubElement(step, 'em')
+       #     em.text = s[0]
+
+        #    details = etree.SubElement(step,'details')
+         #   for detail_stuff in s[1]:
+          #      detail = etree.SubElement(details,'d')
+           #     if isinstance(detail_stuff,list):
+            #        for l in detail_stuff:
+                #        li = etree.SubElement(detail,'l')
+                 #       li.text = l
+              #  else:
+               #     detail.text = detail_stuff
+    
+
+    tipsNode = etree.SubElement(doc,'tips')
+    for tips in all_tips:
+        for tip in tips:
+            tipNode = etree.SubElement(tipsNode,'tip')
+            tipNode.append(tip)
 
     et = etree.ElementTree(root)
     et.write(out,xml_declaration=True, encoding='utf-8',pretty_print=True)
@@ -113,7 +206,7 @@ def main():
             continue
         wikihow_page = open(os.path.join(input_path, wikihow_page_path))
         soup = BeautifulSoup(wikihow_page)
-        format_output(os.path.join(output_path, wikihow_page_path + ".xml"), parse_categories(soup), parse_step_listings(soup))
+        format_output(os.path.join(output_path, wikihow_page_path + ".xml"), parse_categories(soup), parse_step_listings(soup), parse_tips(soup))
 
 if __name__ == "__main__":
     main()
