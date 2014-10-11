3ae9f25f08044a4fed8136130ec86bc08dcedac7
hunterhector <hunterhector@gmail.com>
2014-10-10 21:03:34 -0400

    reverse training and test

--- src/main/java/edu/cmu/cs/lti/how/preprocess/LeaveOutTestSpliter.java
+++ src/main/java/edu/cmu/cs/lti/how/preprocess/LeaveOutTestSpliter.java
@@ -15,11 +15,11 @@ import java.util.Random;
 public class LeaveOutTestSpliter {
     public static void main(String args[]) throws IOException {
         if (args.length < 2) {
-            System.err.println("Provide the data path, base output path and probablity of training data as arguments");
+            System.err.println("Provide the data path, base output path and percentage as training data as arguments");
             System.exit(1);
         }
 
-        File intputDir = new File(args[0]);
+        File inputDir = new File(args[0]);
         File outputDir = new File(args[1]);
         double p = new Double(args[2]);
 
@@ -34,11 +34,11 @@ public class LeaveOutTestSpliter {
         File testDir = new File(outputDir.getAbsolutePath() + "/test/");
 
         for (File src : FileUtils.listFiles(
-                intputDir,
+                inputDir,
                 SUFFIX,
                 true
         )) {
-            if (rand.nextDouble() > p) {
+            if (rand.nextDouble() < p) {
                 FileUtils.copyFileToDirectory(src, trainDir);
             } else {
                 FileUtils.copyFileToDirectory(src, testDir);
