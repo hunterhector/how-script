9b94293aad104b0281bd703d4e5a8c66130efc1b
hunterhector <hunterhector@gmail.com>
2014-12-04 02:56:21 -0500

    Print alignment nicely

--- src/main/java/edu/cmu/cs/lti/how/script/alignment/AlignmentReader.java
+++ src/main/java/edu/cmu/cs/lti/how/script/alignment/AlignmentReader.java
@@ -6,8 +6,11 @@ import edu.cmu.cs.lti.how.model.script.ScriptClusterNode;
 import edu.cmu.cs.lti.how.model.script.ScriptClusterNonTerminalNode;
 import org.apache.commons.lang.SerializationUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,10 +19,44 @@ import java.io.*;
  * Time: 10:01 PM
  */
 public class AlignmentReader {
-    Writer writer;
+    File outDir;
 
-    public AlignmentReader(Writer writer) {
-        this.writer = writer;
+    String basename;
+
+    int counter;
+
+    public AlignmentReader(File outDir, String basename) {
+        this.outDir = outDir;
+        this.basename = basename;
+    }
+
+    public void writeAlignment(List<String[]> alignments, int n) throws IOException {
+        Writer w = new FileWriter(new File(outDir, basename + "_" + counter));
+        counter++;
+
+//        System.out.println("Dumping");
+//        for (String[] alignment : alignments) {
+//            System.out.println(Arrays.toString(alignment));
+//        }
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (String[] alignment : alignments) {
+                sb.append(alignment[i]);
+                sb.append("\t,\t");
+            }
+            sb.append("\n");
+            writeline(w, "", sb.toString());
+        }
+        w.close();
+    }
+
+    public void writeNode(ScriptClusterLeaveNode node) throws IOException {
+        Writer w = new FileWriter(new File(outDir, basename + "_" + counter));
+        counter++;
+        for (String event : node.getEvents()) {
+            writeline(w, "", event);
+        }
     }
 
     public void check(ScriptClusterNode node, int depth) throws IOException {
@@ -27,33 +64,108 @@ public class AlignmentReader {
 
         if (node.isLeave()) {
             ScriptClusterLeaveNode leaveNode = (ScriptClusterLeaveNode) node;
-            for (String event : leaveNode.getEvents()) {
-                writeline(prefix, event);
-            }
+            writeNode(leaveNode);
         } else {
             ScriptClusterNonTerminalNode nonTerminalNode = (ScriptClusterNonTerminalNode) node;
-            writeline(prefix, "Score: " + nonTerminalNode.getAlignScore());
-            writeline(prefix, "Alignment: " + nonTerminalNode.getSource().toString());
-            writeline(prefix, "Branching Left");
-            check(node.getLeft(), depth + 1);
-            writeline(prefix, "Branching Right");
-            check(node.getRight(), depth + 1);
+
+            if (nonTerminalNode.getSource().size() <= 20) {
+//                System.out.println(nonTerminalNode.getObservationCount());
+                writeAlignment(getAlignments(nonTerminalNode), nonTerminalNode.getSource().size());
+            } else {
+                check(node.getLeft(), depth + 1);
+                check(node.getRight(), depth + 1);
+            }
         }
     }
 
-    private void writeline(String prefix, String content) throws IOException {
-        write(prefix, content + "\n");
+    private List<String[]> getAlignments(ScriptClusterNonTerminalNode node) {
+        List<Pair<Integer, Integer>> source = node.getSource();
+
+        int n = source.size();
+
+        int[] leftFrom = new int[n];
+        int[] rightFrom = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            leftFrom[i] = source.get(i).getLeft();
+            rightFrom[i] = source.get(i).getRight();
+        }
+
+//        System.out.println("Left from: " + Arrays.toString(leftFrom));
+
+        List<String[]> leftAlignments = getAlignments(node.getLeft(), n, leftFrom, 1);
+
+//        System.out.println("Right from: " + Arrays.toString(rightFrom));
+
+        List<String[]> rightAlignments = getAlignments(node.getRight(), n, rightFrom, 1);
+
+        List<String[]> alignments = new ArrayList<>();
+        alignments.addAll(leftAlignments);
+        alignments.addAll(rightAlignments);
+        return alignments;
     }
 
-    private void write(String prefix, String content) throws IOException {
+    private List<String[]> getAlignments(ScriptClusterNode node, int n, int[] from, int depth) {
+        String prefix = StringUtils.repeat("  ", depth);
+
+        List<String[]> alignments = new ArrayList<>();
+
+        if (node.isLeave()) {
+            String[] events = new String[n];
+            ScriptClusterLeaveNode lNode = (ScriptClusterLeaveNode) node;
+            for (int i = 0; i < n; i++) {
+                if (from[i] >= 0) {
+                    events[i] = lNode.getEvents()[from[i]];
+                } else {
+                    events[i] = "-";
+                }
+            }
+            alignments.add(events);
+        } else {
+            ScriptClusterNonTerminalNode nonTerminalNode = (ScriptClusterNonTerminalNode) node;
+            List<Pair<Integer, Integer>> sources = nonTerminalNode.getSource();
+
+            int[] leftFrom = new int[n];
+            int[] rightFrom = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                if (from[i] >= 0) {
+//                    System.out.println("From "+ i + " is "+ from[i]);
+                    Pair<Integer, Integer> source = sources.get(from[i]);
+//                    System.out.println(source);
+                    leftFrom[i] = source.getLeft();
+                    rightFrom[i] = source.getRight();
+                } else {
+                    leftFrom[i] = -1;
+                    rightFrom[i] = -1;
+                }
+            }
+//            System.out.println(prefix + "Left from: " + Arrays.toString(leftFrom));
+
+            List<String[]> leftAlignments = getAlignments(node.getLeft(), n, leftFrom, depth + 1);
+
+//            System.out.println(prefix + "Right from: " + Arrays.toString(rightFrom));
+
+            List<String[]> rightAlignments = getAlignments(node.getRight(), n, rightFrom, depth + 1);
+
+            alignments.addAll(leftAlignments);
+            alignments.addAll(rightAlignments);
+        }
+
+        return alignments;
+    }
+
+    private void writeline(Writer writer, String prefix, String content) throws IOException {
+        write(writer, prefix, content + "\n");
+    }
+
+    private void write(Writer writer, String prefix, String content) throws IOException {
         writer.write(prefix + content);
     }
 
     public static void main(String[] args) throws IOException {
         String basename = "alignment_cluster";
-
         File outDir = new File("data/cluster_out");
-
         if (!outDir.exists()) {
             outDir.mkdirs();
         }
@@ -62,10 +174,8 @@ public class AlignmentReader {
 
         for (int i = 0; i < cluster.getCluster().size(); i++) {
             ScriptClusterNode node = cluster.getCluster().get(i);
-            PrintWriter writer = new PrintWriter(new FileWriter(new File(outDir, basename + i)), true);
-            AlignmentReader reader = new AlignmentReader(writer);
+            AlignmentReader reader = new AlignmentReader(outDir, basename + i);
             reader.check(node, 0);
-            writer.close();
         }
     }
 }
